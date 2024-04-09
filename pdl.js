2a554e9b89a7b63ab066e4e65f075a797321406b
G2china <52969689+G2china@users.noreply.github.com>
2024-04-09 22:47:45 +0800

    dge4555ff

--- pdl.js
+++ pdl.js
@@ -3,16 +3,16 @@ console.log("Response Body:", body);
 try {
     var jsonData = JSON.parse(body);
     console.log("Parsed JSON Data:", jsonData);
-    if (jsonData.hasOwnProperty("msg")) {
-        console.log("Value of msg:", jsonData.msg);
-        if (jsonData.msg === "当前已超过限购数量") {
+    if (jsonData.hasOwnProperty("sourceMsg")) {
+        console.log("Value of sourceMsg:", jsonData.sourceMsg);
+        if (jsonData.sourceMsg === "当前已超过限购数量") {
             console.log("Value is '当前已超过限购数量', so outputting '987'");
             console.log("987");
         } else {
             console.log("Value is not '当前已超过限购数量'");
         }
     } else {
-        console.log("No 'msg' key found in JSON data");
+        console.log("No 'sourceMsg' key found in JSON data");
     }
 } catch (error) {
     console.log("Error: " + error.message);
