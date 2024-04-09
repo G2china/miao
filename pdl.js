9881dbe2add3ca6b5ff79a9c181b4d16ec35bbe2
G2china <52969689+G2china@users.noreply.github.com>
2024-04-09 22:29:51 +0800

    6565565

--- pdl.js
+++ pdl.js
@@ -1,11 +1,14 @@
 $notify("提示", "胖东来", "库存加满了");
-// 解析返回值为 JSON
 var body = $response.body;
-var jsonData = JSON.parse(body);
-console.log(jsonData.msg);
-// 检查键 msg 是否等于 "你好啊"
-if (jsonData.msg === "当前已超过限购数量") {
-    console.log("9876545645646564564");
+console.log("8989889877987");
+try {
+    var jsonData = JSON.parse(body);
+	console.log(jsonData.msg);
+    if ( jsonData.msg === "当前已超过限购数量") {
+        console.log("987");
+    }
+} catch (error) {
+    console.log("Error: " + error.message);
+} finally {
+    $done({});
 }
-
-$done({});
