d836aaff29b596718b6bf5541cbab223afe77717
G2china <52969689+G2china@users.noreply.github.com>
2024-04-09 22:41:35 +0800

    9955955

--- pdl.js
+++ pdl.js
@@ -1,11 +1,18 @@
-$notify("提示", "胖东来", "库存加满了");
 var body = $response.body;
-console.log("8989889877987");
+console.log("Response Body:", body);
 try {
     var jsonData = JSON.parse(body);
-	console.log(jsonData.msg);
-    if ( jsonData.msg === "当前已超过限购数量") {
-        console.log("987");
+    console.log("Parsed JSON Data:", jsonData);
+    if (jsonData.hasOwnProperty("msg")) {
+        console.log("Value of msg:", jsonData.msg);
+        if (jsonData.msg === "当前已超过限购数量") {
+            console.log("Value is '当前已超过限购数量', so outputting '987'");
+            console.log("987");
+        } else {
+            console.log("Value is not '当前已超过限购数量'");
+        }
+    } else {
+        console.log("No 'msg' key found in JSON data");
     }
 } catch (error) {
     console.log("Error: " + error.message);
