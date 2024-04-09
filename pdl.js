621ed0689dc754c45d8aac25fcf74d625e1c4460
G2china <52969689+G2china@users.noreply.github.com>
2024-04-09 22:10:46 +0800

    12345

--- pdl.js
+++ pdl.js
@@ -1,15 +1,12 @@
 var body = $response.body;
-console.log("进来了");
 try {
-    // 解析返回值为 JSON
     var jsonData = JSON.parse(body);
-    console.log("去你妹的");
-    // 检查键 msg 是否等于 "你好啊"
-    if (jsonData.msg.toString() === "当前已超过限购数量") {
+	console.log(jsonData.msg);
+    if ( jsonData.msg === "当前已超过限购数量") {
         console.log("987");
     }
 } catch (error) {
-    console.log("Error:", error.message);
+    console.log("Error: " + error.message);
+} finally {
+    $done({});
 }
-
-$done();
