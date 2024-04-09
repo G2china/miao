99bdb99814112cab7e8f1605e1d67baa7e57b73d
G2china <52969689+G2china@users.noreply.github.com>
2024-04-09 13:02:46 +0800

    Update kyy.js

--- kyy.js
+++ kyy.js
@@ -6,9 +6,11 @@ var url3 = "zuduijun.com/api/zdj-activities";
 var url4 = "act=GetCustSubscribeDateAll";
 
 if ($request.url.indexOf(url1) != -1) {
+    //取消开抢时间限制 取消弹窗
     resBody = resBody.replace(/"start_time_info":{.*?}/g, '"start_time_info":null');
     resBody = resBody.replace(/"must_read".*?]}/g, '"must_read":null');
 } else if ($request.url.indexOf(url2) != -1) {
+    //增加可预约库存 提前进
     resBody = resBody.replace(/"vote_limit":.*?,/g, '"vote_limit":666,');
     resBody = resBody.replace(/"is_started":[^}]/g, '"is_started":1');
 } else if ($request.url.indexOf(url3) != -1) {
