d313f66ef8a2c2c5c429e130550c536e63d3037f
G2china <52969689+G2china@users.noreply.github.com>
2024-04-09 23:00:12 +0800

    Update pdl.js

--- pdl.js
+++ pdl.js
@@ -1,6 +1,6 @@
 var body = $response.body;
 var jsonData = JSON.parse(body);
 if ( jsonData.sourceMsg  === "当前已超过限购数量") {
-    $notify("Quantumult X", "胖东来", "超过限购数量");
+    $notify("Quantumult X", "胖东来", "超过限购数量.");
 }
 $done({});
