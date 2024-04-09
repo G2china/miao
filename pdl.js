7b35309631bbf2262df088b8d2b787cc634ec3d0
G2china <52969689+G2china@users.noreply.github.com>
2024-04-09 21:56:12 +0800

    123

--- pdl.js
+++ pdl.js
@@ -1,9 +1,15 @@
-// 解析返回值为 JSON
-var jsonData = JSON.parse($response.body);
+var body = $response.body;
 
-// 检查键 msg 是否等于 "你好啊"
-if (jsonData.msg && jsonData.msg.toString() === "当前已超过限购数量") {
-    console.log("当前已超过限购数量");
+try {
+    // 解析返回值为 JSON
+    var jsonData = JSON.parse(body);
+
+    // 检查键 msg 是否等于 "你好啊"
+    if (jsonData.msg && jsonData.msg.toString() === "你好啊") {
+        console.log("987");
+    }
+} catch (error) {
+    console.log("Error:", error.message);
 }
 
 $done();
