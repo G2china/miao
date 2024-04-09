3e3debb4ab39ef882d56d4170970ed2d1352b660
G2china <52969689+G2china@users.noreply.github.com>
2024-04-09 22:00:18 +0800

    123

--- pdl.js
+++ pdl.js
@@ -3,9 +3,9 @@ var body = $response.body;
 try {
     // 解析返回值为 JSON
     var jsonData = JSON.parse(body);
-
+    console.log("去你妹的");
     // 检查键 msg 是否等于 "你好啊"
-    if (jsonData.msg && jsonData.msg.toString() === "你好啊") {
+    if (jsonData.msg.toString() === "当前已超过限购数量") {
         console.log("987");
     }
 } catch (error) {
