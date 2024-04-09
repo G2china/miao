79700ec62865d613e20fd9a29d9a35a82a08cfa1
G2china <52969689+G2china@users.noreply.github.com>
2024-04-09 22:52:55 +0800

    2449

--- pdl.js
+++ pdl.js
@@ -1,21 +1,5 @@
 var body = $response.body;
-console.log("Response Body:", body);
-try {
-    var jsonData = JSON.parse(body);
-    console.log("Parsed JSON Data:", jsonData);
-    if (jsonData.hasOwnProperty("sourceMsg")) {
-        console.log("Value of sourceMsg:", jsonData.sourceMsg);
-        if (jsonData.sourceMsg === "当前已超过限购数量") {
-            console.log("Value is '当前已超过限购数量', so outputting '987'");
-            console.log("987");
-        } else {
-            console.log("Value is not '当前已超过限购数量'");
-        }
-    } else {
-        console.log("No 'sourceMsg' key found in JSON data");
-    }
-} catch (error) {
-    console.log("Error: " + error.message);
-} finally {
-    $done({});
+var jsonData = JSON.parse(body);
+if ( jsonData.sourceMsg  === "当前已超过限购数量") {
+    $notify("Quantumult X", "胖东来", "超过限购数量");
 }
