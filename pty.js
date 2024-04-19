858c6c515662a3615c86159147f34520c74b5c83
G2china <52969689+G2china@users.noreply.github.com>
2024-04-19 22:07:29 +0800

    Update pty.js

--- pty.js
+++ pty.js
@@ -1,8 +1,23 @@
-$notify("Quantumult X", "通知副标题", "这是一条通知内容");
+var resBody = $response.body;
+// 定义网址变量
+var url1 = "https://api.pintuanya.com/api/v1/common/groupon/detail?groupon_uuid=";
+var url2 = "api.pintuanya.com/api/v1/common/groupon/item/batch?groupon_uuid=";
+var url3 = "api.pintuanya.com/api/common/previewOrder2";
 
-const body = $response.body;
+// 拼团详情，将 state 改为 2
+if ($request.url.indexOf(url1) != -1) {
+    resBody = resBody.replace(/"state":[^,]*/g, '"state":2');
+    $done({body: resBody});
+}
 
-// 对响应 body 中的 "state" 进行替换操作2233
-const replacedBody = body.replace(/\"state\":[^,]/g, '"state":2');
+// 取消冻结商品，将 is_locked 改为 0
+if ($request.url.indexOf(url2) != -1) {
+    resBody = resBody.replace(/"is_locked":1/g, '"is_locked":0');
+    $done({body: resBody});
+}
 
-$done({ body: replacedBody });
+// 预览订单，将 locked 改为 ok
+if ($request.url.indexOf(url3) != -1) {
+    resBody = resBody.replace(/locked/g, 'ok');
+    $done({body: resBody});
+}
