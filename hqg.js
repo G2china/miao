7e46cc0e18780fb5d44f52eae736b6d3d219a427
G2china <52969689+G2china@users.noreply.github.com>
2024-04-23 10:35:31 +0800

    Update hqg.js

--- hqg.js
+++ hqg.js
@@ -1,5 +1,12 @@
-if ($request.url.indexOf("mall.hqc.cn/flash-sale/site-protocol/get-protocol?m_code=") != -1) {
-    var resBody = $response.body;
-    resBody = resBody.replace(/"alertFlag":true/g, '"alertFlag":false');
-    $done({body: resBody});
+var resBody = $response.body;
+var url1 = "mall.hqc.cn/flash-sale/site-protocol/get-protocol?m_code=";
+var url2 = "mall.hqc.cn/flash-sale/perform-reserve/list-perform-schedule-date";
+if ($request.url.indexOf(url1) != -1) {
+	//取消弹窗
+    resBody = resBody.replace(/"alertFlag":true/g, '"alertFlag":false'); 
+}else if ($request.url.indexOf(url2) != -1) {
+	//增加日期
+    var newData = ',"2024-05-03"';
+    resBody = resBody.replace(/"2024-05-02"/g, '"2024-05-02"' + newData);
 }
+$done({ body: resBody });
