40b5f707dac60885566af202be7c844851f6c3d8
G2china <52969689+G2china@users.noreply.github.com>
2024-04-30 13:04:33 +0800

    Update shtwg.js

--- shtwg.js
+++ shtwg.js
@@ -2,7 +2,7 @@ if ($request.url.indexOf("getReservedateListByStadiumId.xhtml") != -1) {
     var resBody = $response.body;
     var json = JSON.parse(resBody);
     var endDate = json.data.enddate;
-    var date = {"reservedate": endDate, "visitorsnum": 3123, "usednum": null, "avaiablenum": null};
+    var date = {"reservedate": endDate, "visitorsnum": 3200, "usednum": null, "avaiablenum": null};
     json.data.resultList.push(date);
     console.log(JSON.stringify(json));
     $done({ body: JSON.stringify(json) });
