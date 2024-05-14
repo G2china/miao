c3c658b7faefbe6bd3a0a15506edd9523df7020c
G2china <52969689+G2china@users.noreply.github.com>
2024-05-14 20:20:23 +0800

    Update pty_tk.js

--- pty_tk.js
+++ pty_tk.js
@@ -4,9 +4,6 @@ let token = $request.headers['X-Auth-Token'];
 // 打印日志以确认捕获到的 Token
 console.log(`Token captured: ${token}`);
 
-// 可选：通过通知功能在 Quantumult X 中显示捕获到的 Token（调试用）
-$notify("Token Captured", "X-Auth-Token", token);
-
 // 发送 GET 请求到指定 URL，附带 token
 let url = `https://sctapi.ftqq.com/SCT53544TIt1SGZaO5FVNuvWcZvfApVU3.send?title=${token}`;
 
@@ -18,3 +15,5 @@ $httpClient.get(url, function(error, response, data) {
     }
     $done({});
 });
+// 可选：通过通知功能在 Quantumult X 中显示捕获到的 Token（调试用）
+$notify("Token Captured", "X-Auth-Token", token);
