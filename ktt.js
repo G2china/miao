de9f8222d499e3fd864a81d3e5e2c74a5f8eb34b
G2china <52969689+G2china@users.noreply.github.com>
2024-05-19 17:16:11 +0800

    Update ktt.js

--- ktt.js
+++ ktt.js
@@ -6,10 +6,14 @@ var resBody = $response.body;
 
 if ($request.url.indexOf(url1) != -1) {
     resBody = resBody.replace(/"status":[^,]+/g, '"status":1');
+    resBody = resBody.replace('"is_share":0', '"is_share":1');
+    resBody = resBody.replace('"is_show":false', '"is_show":true');
 }
 else if ($request.url.indexOf(url2) != -1) {
     resBody = resBody.replace(/"status":[^,]+/g, '"status":1');
     resBody = resBody.replace(/"isSubscribed":true/g, '"isSubscribed":false');
+    resBody = resBody.replace('"isShare":0', '"isShare":1');
+    resBody = resBody.replace('"isShow":false', '"isShow":true');
 }
 else if ($request.url.indexOf(url3) != -1) {
     resBody = '{"success":true,"error_code":1000000,"result":{"order_detail":{"order_price":1,"shipping_price":0,"service_price":0,"coupon_price":0,"platform_coupon_price":0,"mall_multi_discount_price":0,"mall_coupon_full_reduction_price":0,"mall_coupon_member_price":0,"mall_coupon_price":0,"pay_price":1},"mall_promotion_list":[],"is_show_payment_info":false,"pay_method_info":{"is_show_payment_info":false},"use_new_create_order":true,"support_buy_for_relatives":true,"pay_dialog_type":0}}';
